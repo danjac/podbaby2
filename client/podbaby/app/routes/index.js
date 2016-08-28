a97fa19f57f940fea18dc0351042f7de094446f2
Dan Jacob <danjac354@gmail.com>
2016-08-28 23:23:29 +0300

    Pagination

--- client/podbaby/app/routes/index.js
+++ client/podbaby/app/routes/index.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.get('store').query('episode', {
-      page: 1
-    });
+  queryParams: {
+    page: {
+      refreshModel: true
+    }
+  },
+  model(params) {
+    return this.get('store').query('episode', params);
   }
 });
