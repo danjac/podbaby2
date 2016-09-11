f449b6828937a4cca3f5c5dba98d293086c8290d
Dan Jacob <danjac354@gmail.com>
2016-09-11 21:53:12 +0300

    Adding tests for model

--- server/podbaby/podcasts/tests.py
+++ server/podbaby/podcasts/tests.py
@@ -36,6 +36,25 @@ class MockRequests:
         return MockResponse()
 
 
+class EpisodeTests(TestCase):
+
+    def test_get_stream_url_if_none(self):
+        """
+        Should just be None if no enclosure url
+        """
+
+        e = Episode()
+        self.assertEqual(e.get_stream_url(), None)
+
+    def test_get_stream_url(self):
+
+        e = Episode(
+            pk=1000,
+            enclosure_url='http://www.libsyn.com/podcast_160911.mp3'
+        )
+        self.assertEqual(e.get_stream_url(), '/stream/1000.mp3')
+
+
 @mock.patch('podcasts.models.requests', MockRequests)
 class ChannelTests(TestCase):
 
