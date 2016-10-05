17fac20498ed8077cc0f1e9c4c07237e65d951cd
Dan Jacob <danjac354@gmail.com>
2016-10-06 01:32:50 +0300

    Wiring up events

--- client/podbaby/src/routes/latest.js
+++ client/podbaby/src/routes/latest.js
@@ -4,8 +4,13 @@ import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router';
 
 import { fetchEpisodes } from '../modules/episodes';
-import { episodesSelector } from '../selectors';
+import { addBookmark, removeBookmark } from '../modules/auth';
+import { startPlayer, stopPlayer } from '../modules/player';
 
+import { episodesSelector } from '../selectors';
+import { parsePageNumberFromUrl } from '../utils/pagination';
+
+import Loader from '../components/loader';
 import EpisodeList from '../components/episode-list';
 
 class LatestEpisodes extends Component {
@@ -13,6 +18,10 @@ class LatestEpisodes extends Component {
   constructor(props) {
     super(props);
     this.handleSelectPage = this.handleSelectPage.bind(this);
+    this.handleStartPlayer = this.handleStartPlayer.bind(this);
+    this.handleStopPlayer = this.handleStopPlayer.bind(this);
+    this.handleAddBookmark = this.handleAddBookmark.bind(this);
+    this.handleRemoveBookmark = this.handleRemoveBookmark.bind(this);
   }
 
   componentDidMount() {
@@ -39,29 +48,49 @@ class LatestEpisodes extends Component {
     this.props.actions.onFetchEpisodes(url);
   }
 
-  handleSelectPage() {
+  handleSelectPage(url) {
+    const page = parsePageNumberFromUrl(url);
+    this.props.router.replace({
+      query: {
+        page: page,
+      }
+    });
   }
 
-  handleStartPlayer() {
+  handleStartPlayer(episode) {
+    this.props.actions.onStartPlayer(episode);
   }
 
-  handleStopPlayer() {
+  handleStopPlayer(episode) {
+    this.props.actions.onStopPlayer(episode);
   }
 
-  handleAddBookmark() {
+  handleAddBookmark(episode) {
+    this.props.actions.onAddBookmark(episode);
   }
 
-  handleRemoveBookmark() {
+  handleRemoveBookmark(episode) {
+    this.props.actions.onRemoveBookmark(episode);
   }
 
   render() {
 
-    const { episodes, next, previous } = this.props;
+    const {
+      episodes,
+      next,
+      previous,
+      isLoggedIn,
+      isLoading } = this.props;
+
+    if (isLoading) {
+      return <Loader />;
+    }
 
     return (
         <EpisodeList episodes={episodes}
                      next={next}
                      previous={previous}
+                     isLoggedIn={isLoggedIn}
                      onSelectPage={this.handleSelectPage}
                      onStartPlayer={this.handleStartPlayer}
                      onStopPlayer={this.handleStopPlayer}
@@ -72,9 +101,14 @@ class LatestEpisodes extends Component {
 }
 
 LatestEpisodes.propTypes = {
+  actions: PropTypes.objectOf(PropTypes.func).isRequired,
   episodes: PropTypes.array.isRequired,
   next: PropTypes.string,
   previous: PropTypes.string,
+  router: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = state => {
@@ -105,6 +139,10 @@ const mapDispatchToProps = dispatch => {
   return {
     actions: bindActionCreators({
       onFetchEpisodes: fetchEpisodes,
+      onStartPlayer: startPlayer,
+      onStopPlayer: stopPlayer,
+      onAddBookmark: addBookmark,
+      onRemoveBookmark: removeBookmark,
     }, dispatch)
   };
 };
