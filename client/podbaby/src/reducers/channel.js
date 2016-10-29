adc31e77371c0aa840ddfa1a8ba30d9ce3b69afd
Dan Jacob <danjac354@gmail.com>
2016-10-29 12:30:44 +0300

    Channel reducer

---
+++ client/podbaby/src/reducers/channel.js
@@ -0,0 +1,39 @@
+import {
+  FETCH_CHANNEL_FAILURE,
+  FETCH_CHANNEL_REQUEST,
+  FETCH_CHANNEL_SUCCESS,
+} from '../action-types';
+
+const initialState = {
+  loading: false,
+  channel: null,
+  error: null,
+};
+
+export default function(state = initialState, action) {
+
+  switch (action.type) {
+
+    case FETCH_CHANNEL_REQUEST:
+      return {...state,
+        loading: true,
+        error: null,
+      };
+
+    case FETCH_CHANNEL_SUCCESS:
+      return {...state,
+        loading: false,
+        channel: action.payload
+      };
+
+    case FETCH_CHANNEL_FAILURE:
+      return {...state,
+        loading: false,
+        error: action.error
+      };
+
+    default:
+      return state;
+
+  }
+}
---
+++ client/podbaby/src/reducers/channel.test.js
@@ -0,0 +1,52 @@
+import {
+  FETCH_CHANNEL_FAILURE,
+  FETCH_CHANNEL_REQUEST,
+  FETCH_CHANNEL_SUCCESS,
+} from '../action-types';
+
+import reducer from './channel';
+
+it('should handle FETCH_CHANNEL_REQUEST', () => {
+  const state = reducer({
+    loading: false,
+  }, {
+    type: FETCH_CHANNEL_REQUEST,
+  });
+
+  expect(state.loading).toEqual(true);
+
+});
+
+it('should handle FETCH_CHANNEL_FAILURE', () => {
+  const error = new Error('No channel found');
+
+  const state = reducer({
+    loading: true,
+    channel: null,
+    error: null,
+  }, {
+    type: FETCH_CHANNEL_FAILURE,
+    error,
+  });
+
+  expect(state.loading).toEqual(false);
+  expect(state.channel).toEqual(null);
+  expect(state.error).toEqual(error);
+});
+
+
+it('should handle FETCH_CHANNEL_SUCCESS', () => {
+  const state = reducer({
+    loading: true,
+    channel: null,
+  }, {
+    type: FETCH_CHANNEL_SUCCESS,
+    payload: {
+      id: 1,
+    },
+  });
+
+  expect(state.loading).toEqual(false);
+  expect(state.channel.id).toEqual(1);
+
+});
