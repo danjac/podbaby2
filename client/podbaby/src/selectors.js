e48c5e3143d7347aaa30ee3c084058cddb84f0a7
Dan Jacob <danjac354@gmail.com>
2016-10-30 22:30:47 +0200

    Added selectors

---
+++ client/podbaby/src/selectors.js
@@ -0,0 +1,35 @@
+import {
+  createSelector,
+} from 'reselect';
+
+const subscriptionsSelector = state => state.subscriptions;
+const bookmarksSelector = state => state.bookmarks;
+const playerSelector = state => state.player;
+
+export const channelsSelector = createSelector(
+  state => state.channels.results,
+  subscriptionsSelector,
+  (channels, subscriptions) => {
+    return channels.map(channel => {
+      return {...channel,
+        subscribed: subscriptions.includes(channel.id),
+      };
+    });
+  },
+);
+
+export const episodesSelector = createSelector(
+  state => state.episodes.results,
+  playerSelector,
+  subscriptionsSelector,
+  bookmarksSelector,
+  (episodes, player, subscriptions, bookmarks) => {
+    return episodes.map(episode => {
+      return {...episode,
+        subscribed: subscriptions.includes(episode.channel.id),
+        bookmarked: bookmarks.includes(episode.id),
+        playing: player.playing && player.episode && player.episode.id === episode.id,
+      };
+    });
+  },
+);
