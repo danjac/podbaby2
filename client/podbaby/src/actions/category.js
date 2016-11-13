f448d0db7f73dd19ef7183b313b0616ac2328593
Dan Jacob <danjac354@gmail.com>
2016-11-13 13:47:46 +0200

    Category detail

---
+++ client/podbaby/src/actions/category.js
@@ -0,0 +1,28 @@
+import * as api from '../api';
+import { dispatchApiCall } from './utils';
+
+import {
+  FETCH_CATEGORY_FAILURE,
+  FETCH_CATEGORY_REQUEST,
+  FETCH_CATEGORY_SUCCESS,
+  FETCH_CHANNELS_FAILURE,
+  FETCH_CHANNELS_REQUEST,
+  FETCH_CHANNELS_SUCCESS,
+} from '../actionTypes';
+
+export const fetchCategory = id => dispatch => dispatchApiCall(
+  dispatch,
+  api.categories.get(id),
+  FETCH_CATEGORY_REQUEST,
+  FETCH_CATEGORY_SUCCESS,
+  FETCH_CATEGORY_FAILURE,
+);
+
+
+export const fetchChannelsForCategory = (id, page, searchQuery) => dispatch => dispatchApiCall(
+  dispatch,
+  api.categories.fetchChannels(id, page, searchQuery),
+  FETCH_CHANNELS_REQUEST,
+  FETCH_CHANNELS_SUCCESS,
+  FETCH_CHANNELS_FAILURE,
+);
