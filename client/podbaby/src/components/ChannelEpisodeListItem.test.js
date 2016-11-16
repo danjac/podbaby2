5db930619385eeca5617fc2329c764c76dd7e8b0
Dan Jacob <danjac354@gmail.com>
2016-11-16 19:06:29 +0200

    More tests

---
+++ client/podbaby/src/components/ChannelEpisodeListItem.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import * as bs from 'react-bootstrap';
+
+import {
+  mockEpisode,
+  mockEpisodeActions,
+} from '../test-utils/mocks';
+
+import { ChannelEpisodeListItem } from './ChannelEpisodeListItem';
+
+const defaultProps = () => {
+  return {
+    episode: mockEpisode(),
+    authenticated: false,
+    ...mockEpisodeActions(),
+  };
+};
+
+it('should render the component', () => {
+  // smoke test
+  const props = defaultProps();
+  const rendered = shallow(<ChannelEpisodeListItem {...props} />);
+  const panel = rendered.find(bs.Panel);
+  expect(panel.prop('header')).toBeDefined();
+});
--- client/podbaby/src/components/utils.js
+++ client/podbaby/src/components/utils.js
@@ -10,12 +10,15 @@ const sanitizeOptions = {
 };
 
 export const sanitize = dirty => {
+  if (!dirty) {
+    return { __html: '' };
+  }
   // cleans HTML allowing only specific tags
   return {
     __html: sanitizeHtml(dirty.replace('\n', '<br>'), sanitizeOptions),
   };
 };
 
-export const formatDate = (date, format='MMMM Do YYYY') => date && moment(date).format(format);
+export const formatDate = (date, format = 'MMMM Do YYYY') => date && moment(date).format(format);
 
 export const timeSince = date => date && moment(date).fromNow();
